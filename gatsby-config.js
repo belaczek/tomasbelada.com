54031927e2971fa38974d1615e3699e1270c4bc5
Tomas Belada <tomasbelada@hotmail.com>
2019-04-07 22:19:44 +0200

    chore(deployment): add now.json config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Tomas Belada`,
+    description: `Homesite of Tomas Belada, the not very famous Javascript/Typescript engineer. At least so far...`,
+    author: `Tomas Belada <tomas@belada.net>`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,11 +15,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-typography`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Tomas Belada`,
+        short_name: `tomasbelada.com`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
